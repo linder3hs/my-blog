0e3bba04bac6a167f901b4797a53c401c48d35fa
linder3hs <linderhassinger00@gmail.com>
2022-07-14 02:04:28 -0500

    add api

---
+++ pages/api/blog.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { BASE_URL } from "./service";
+
+type Data = {
+  name: string;
+  description: string;
+  tags: string[];
+  image: string;
+  read_time: string;
+};
+
+type ErrorRes = {
+  ok: boolean;
+  error: string;
+};
+
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<Data[] | ErrorRes>
+) {
+  try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+    res.status(200).json(json);
+  } catch (error: any) {
+    res.status(500).json({
+      ok: false,
+      error: error.message,
+    });
+  }
+}
---
+++ pages/api/service.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://linder3hs.github.io/blog-json/blog.json";
