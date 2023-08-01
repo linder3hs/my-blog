cb66a0c16620d89ec94e49dbfb4bd225d1b03c4b
linder3hs <linderhassinger00@gmail.com>
2023-08-01 00:13:50 -0500

    improve design

---
+++ assets/index.ts
@@ -0,0 +1,4 @@
+export { default as octocat } from "./icons/octocat.svg";
+export { default as laptop } from "./icons/laptop.svg";
+export { default as market } from "./icons/market.svg";
+export { default as company } from "./icons/company.svg";
