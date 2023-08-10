d1251d63b5d9f3690ac73af0d18cb072f020b81b
linder3hs <linderhassinger00@gmail.com>
2023-08-10 18:25:44 -0500

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,16 +25,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="language" content="Spanish" />
         <meta name="author" content="Linder Hassinger" />
         <meta name="robots" content="noindex,nofollow" />
-        <meta property="og:title" content="Tech Conf" />
+        <meta property="og:title" content="Linder Anderson Hassinger" />
         <meta property="og:type" content="web" />
         <meta property="og:url" content="https://conferences-ten.vercel.app/" />
         <meta
           property="og:image"
-          content="https://nvpaknjxdglnrkhahnaq.supabase.co/storage/v1/object/sign/images/logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvbG9nby5wbmciLCJpYXQiOjE2NzczNTk5OTIsImV4cCI6MTAwMDAwMDAwMTY3NzM2MDAwMH0.Ypmo1QtdQkwf8R0pwn_hIUozy6RClAsUp4YOOJg1uiA&t=2023-02-25T21%3A19%3A52.608Z"
+          content="https://avatars.githubusercontent.com/u/20673011?v=4"
         />
         <meta
           property="og:description"
-          content="Únete a nuestra comunidad y se parte de las mejores charlas de Tech. Encontraras invitados de todo latinoamerica donde nos contaran sus experiencias."
+          content="Soy instructor de software, me gusta enseñar y aprender de los demás."
         />
       </Head>
       <Component {...pageProps} />
